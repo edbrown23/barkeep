b014bdd8e37baa2f6b7a2e1712c4c59da20e7597
Eric Brown <ebrown@salsify.com>
2022-01-23 14:42:28 -0500

    Add some more category stuff
    Plus pull the cocktails table into a partial

--- app/controllers/reagent_categories_controller.rb
+++ app/controllers/reagent_categories_controller.rb
@@ -5,5 +5,8 @@ class ReagentCategoriesController < ApplicationController
 
   def show
     @category = ReagentCategory.find(params[:id])
+
+    amounts = ReagentAmount.where(reagent_category: @category)
+    @cocktails = Recipe.where(id: amounts.pluck(:recipe_id))
   end
 end
