55286a4570d8424f87e4ae7278edffd87b3e08db
Eric Brown <ebrown@salsify.com>
2022-12-06 18:25:07 -0500

    Add a grossly inefficient make-able shared cocktails table
    I mostly added this to highlight how bad the tags related
    queries are so I can fix them

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @available_cocktails = Recipe.for_user(current_user).all_available(current_user)
+    @users_available = Recipe.for_user(current_user).all_available(current_user)
+    @shared_available = Recipe.for_user(nil).all_available(current_user)
+
+    @available_cocktails = @users_available + @shared_available
   end
 end
