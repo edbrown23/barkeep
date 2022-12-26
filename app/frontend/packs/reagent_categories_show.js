eb8285f123609e69fb9e36a414815bfcebb223b5
Eric Brown <ebrown@salsify.com>
2022-12-26 17:34:53 -0500

    Wire up some buttons in the categories show page

--- app/frontend/packs/reagent_categories_show.js
+++ app/frontend/packs/reagent_categories_show.js
@@ -1,4 +1,55 @@
+// TODO: should move all this into a shared lib
+function handleFavorited(event) {
+  const favoritedId = event.detail[0]['favorited_id'];
+
+  const toFavorite = document.querySelector(`#cocktail_${favoritedId}`);
+  if (toFavorite === null) {
+    return;
+  }
+
+  let column = toFavorite.querySelector(`.favorite-column`);
+  column.innerHTML = '&#x2B50;';
+}
+
+function handleUnFavorited(event) {
+  const favoritedId = event.detail[0]['favorited_id'];
+
+  const toFavorite = document.querySelector(`#cocktail_${favoritedId}`);
+  if (toFavorite === null) {
+    return;
+  }
+
+  let column = toFavorite.querySelector(`.favorite-column`);
+  column.innerHTML = '';
+}
+
 window.addEventListener("turbolinks:load", () => {
   lib.load_availability('/shared_cocktails_async');
   lib.load_availability('/cocktails_async');
+
+  const cocktailTable = document.querySelector("#cocktailsTable");
+
+  if (cocktailTable === null) {
+    return;
+  }
+
+  cocktailTable.addEventListener("ajax:success", (event) => {
+    const action = event.detail[0]['action'];
+
+    switch (action) {
+      case "deleted":
+        handleDeletion(event);
+        break;
+      case "favorited":
+        handleFavorited(event);
+        break;
+      case "unfavorited":
+        handleUnFavorited(event);
+        break;
+    }
+  });
+
+  cocktailTable.addEventListener("ajax:error", () => {
+    cocktailTable.insertAdjacentHTML("beforeend", "<p>ERROR</p>");
+  });
 })
