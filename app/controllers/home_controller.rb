87f3f999e3b970a6c962a6ef18908ec596d9c79c
Eric Brown <ebrown@salsify.com>
2022-12-26 17:10:29 -0500

    Add a logged out page with barkeep info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,22 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
 
   def index
+    if user_signed_in?
+      logged_in_index
+    else
+      logged_out_index
+    end
+  end
+
+  def logged_in_index
     cocktails = CocktailAvailabilityService.new(Recipe.where(category: 'cocktail'), current_user)
     models = Recipe.where(id: cocktails.makeable_ids)
     @users_available = models.for_user(current_user)
     @shared_available = models.for_user(nil)
 
     @available_cocktails = @users_available + @shared_available
+  end
+
+  def logged_out_index
   end
 end
