f380e58a6801526e9409e06695244fd419f093ab
Eric Brown <ebrown@salsify.com>
2023-01-14 11:18:33 -0500

    Fix navbar js to work in nested pages

--- app/frontend/packs/navbar.js
+++ app/frontend/packs/navbar.js
@@ -1,6 +1,8 @@
 window.addEventListener('turbolinks:load', () => {
-  const path = window.location.pathname;
+  const path = window.location.pathname.split('/')[1];
 
-  const navbarLink = document.querySelector(`a[href='${path}']`);
-  navbarLink.classList.add('active');
+  const navbarLink = document.querySelector(`a[href='/${path}']`);
+  if (navbarLink) {
+    navbarLink.classList.add('active');
+  }
 });
