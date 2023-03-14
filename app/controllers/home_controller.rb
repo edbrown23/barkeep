ef3015a1bcec9a604a9ef6bebbe3d29b15dd6b90
Eric Brown <ebrown@salsify.com>
2023-03-13 22:27:44 -0400

    Hide ephemeral cocktails from the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ class HomeController < ApplicationController
   def logged_in_index
     cocktails = CocktailAvailabilityService.new(Recipe.where(category: 'cocktail'), current_user)
     models = Recipe.where(id: cocktails.makeable_ids).includes(:reagent_amounts)
-    @users_available = models.for_user(current_user)
+    @users_available = models.for_user(current_user).where('source != ALL(?::varchar[])', '{drink_builder}')
     @shared_available = models.for_user(nil)
 
     @available_cocktails = @users_available + @shared_available
