a5aa2d206b2b102d070d98a928645b3525cd1b9a
Eric Brown <ebrown@salsify.com>
2023-03-15 19:33:49 -0400

    Enable all the rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Barkeep
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -20,5 +20,7 @@ module Barkeep
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.active_record.schema_format = :sql
+
+    config.active_support.cache_format_version = 7.0
   end
 end
