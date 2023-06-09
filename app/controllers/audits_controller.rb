5c74fc13d5e2e935ec47ff09dc59285ce99783b4
Eric Brown <ebrown@salsify.com>
2023-06-09 15:05:03 -0400

    Add some more analysis views to history

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -3,7 +3,20 @@ class AuditsController < ApplicationController
   before_action :set_audit, only: [:show, :edit, :update]
 
   def index
-    @audits = Audit.for_user(current_user).includes(:recipe).order(created_at: :desc).page(params[:page])
+    @filtered_to = nil
+    initial_scope = Audit.for_user(current_user).includes(:recipe)
+    if filter_params[:recipe_id].present?
+      initial_scope = initial_scope.where(recipe_id: filter_params[:recipe_id])
+      @filtered_to = Recipe.find(filter_params[:recipe_id])
+    end
+
+    @audits = initial_scope.order(created_at: :desc).page(params[:page])
+    @counts = initial_scope.group(:recipe).order(count: :desc).count
+    # I'm bad at MVC. Should this be a method on the model?
+    raw_ratings = ActiveRecord::Base.connection.execute(
+      "select recipe_id, max((info->'rating'->>'star_count')::bigint) from audits where user_id = #{current_user.id} group by recipe_id;"
+    )
+    @ratings = raw_ratings.index_by { |r| r['recipe_id'] }
   end
 
   def show
@@ -32,5 +45,9 @@ class AuditsController < ApplicationController
 
   def set_audit
     @audit = Audit.for_user(current_user).find(params[:id])
+  end
+
+  def filter_params
+    params.permit(:recipe_id)
   end
 end
