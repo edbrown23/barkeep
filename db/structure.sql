aa49390aa4ec9dde8d17675a2c66a8809edb15c2
Eric Brown <eric.d.brown23@gmail.com>
2023-08-22 15:16:38 -0400

    Weird sql dump diff that I think is harmless

--- db/structure.sql
+++ db/structure.sql
@@ -10,13 +10,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
--- *not* creating schema, since initdb creates it
-
-
---
 -- Name: extract_tags_from_blob(jsonb); Type: FUNCTION; Schema: public; Owner: -
 --
 
