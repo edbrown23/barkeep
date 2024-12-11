1cc54125caebd01bcaa178104453b63b3550b6ae
Eric Brown <eric.d.brown23@gmail.com>
2024-12-11 13:51:10 -0500

    Lower the sampling rate significantly

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,7 +6,7 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for tracing.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  config.traces_sample_rate = 0.05
   # or
   config.traces_sampler = lambda do |context|
     true
@@ -14,5 +14,5 @@ Sentry.init do |config|
   # Set profiles_sample_rate to profile 100%
   # of sampled transactions.
   # We recommend adjusting this value in production.
-  config.profiles_sample_rate = 1.0
+  config.profiles_sample_rate = 0.05
 end
